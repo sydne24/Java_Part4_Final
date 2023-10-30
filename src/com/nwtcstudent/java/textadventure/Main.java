54fb7d938112931acc8b623cf98a631369b25c44
Nathaniel <nathanielparise@gmail.com>
2023-10-30 14:20:51 -0500

    Re-added required packages and classes, added try/catch/finally block

---
+++ src/com/nwtcstudent/java/textadventure/Main.java
@@ -0,0 +1,36 @@
+package com.nwtcstudent.java.textadventure;
+
+// Nathaniel was here
+
+/**
+ * The Main class is the first to run on program startup, and performs any non-game-related tasks before creating the game controller
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			Controller controller = new Controller();
+		}
+		catch (Exception e) {
+			
+			System.out.println(
+					"An error has occured and the application had to exit!" +
+					"\n\nCause of Error: " + e.getMessage() +
+					"\n\nLocation:\n\t" + e.getStackTrace()[0] +
+					"\n\nFull Stack Trace:"
+					);
+			for (StackTraceElement line : e.getStackTrace()) {
+				
+				System.out.println("\t" + line);
+			}
+		}
+		finally {
+			
+			System.exit(0);
+		}
+		
+	}
+
+}
