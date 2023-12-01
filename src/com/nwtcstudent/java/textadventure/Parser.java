31140c53c46f68282cf3891f19d8ed21f7d30ba4
kejaeg <149027242+kejaeg@users.noreply.github.com>
2023-12-01 15:53:08 -0600

    Update Parser.java
    Progressively adding functionality to the verb-noun parser. Class is currently incomplete
    
    * added variable fields to be returned from GetInput() parse method
    * added string manipulation to convert input to lowercase, trim extra spaces from beginning and end, and split strings by space character
    * GetInput() method updated to return an array with one noun and one verb

--- src/com/nwtcstudent/java/textadventure/Parser.java
+++ src/com/nwtcstudent/java/textadventure/Parser.java
@@ -3,19 +3,35 @@ package com.nwtcstudent.java.textadventure;
 public class Parser {
 	
 	// Input Parser - for now just returns input in lowercase format
-	public static String GetInput() {
+	public static String[] GetInput() {
 		boolean matchFound = false;
+		String noun = "";
+		String verb = "";
+		String[] phrase = new String[2]; //creates array to store noun & verb in
+		//TODO: consider returning embedded class with noun and verb properties instead of an array - get team feedback on preference
 		
 		//while (!matchFound) {
-			String input = Controller.myScan.nextLine();
+		String input = Controller.myScan.nextLine(); //get input
+		input.toString().toLowerCase(); //converts input to lowercase
+		input.trim(); //remove leading and ending white space, if applicable
+		String cleanedInput[] = input.split("//s"); //creates array with parts of input split by space
 			
-	    	if (input.toString().toLowerCase() == "exit") {
+		//TODO: need to account for multiple verb/noun inputs
+			//include prompt for which they'd like to use?
+		
+		//loop through split input array
+		for (int i = 0; i < cleanedInput.length; i++) {
+			//TODO: scan cleanedInput array and assign to variables
+		}
+		
+		//TODO: move or delete -- depending on where functionality will be written
+	    if (input.toString().toLowerCase() == "exit") {
 	    		Controller.endGame();
-	    	}
+	    }
 	    	
 
 		//}
-    	return input.toString().toLowerCase();
+    	return phrase; //returns 2-item array with noun and verb
 	}
 
 }
