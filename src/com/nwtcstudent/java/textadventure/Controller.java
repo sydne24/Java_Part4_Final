fb5cfcc8028d67e3aa2133b42a5273870ec72fd2
kejaeg <149027242+kejaeg@users.noreply.github.com>
2023-12-05 19:02:43 -0600

    Dictionary Updates
    added common commands to parser dictionary

--- src/com/nwtcstudent/java/textadventure/Controller.java
+++ src/com/nwtcstudent/java/textadventure/Controller.java
@@ -337,7 +337,7 @@ public class Controller {
  	 */
  	public void initializeItemLibrary() {
  		for (Item i : Controller.itemList.values()) {
- 			Parser.itemLib.add(i.getName());
+ 			Parser.itemLib.add(i.getName().toLowerCase());
  		}
  	}
  	
